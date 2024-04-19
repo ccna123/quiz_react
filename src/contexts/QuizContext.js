f2dcd26ceabf9ae243415cdb4d3f25ab5be5aebc
THANH <95860402+ccna123@users.noreply.github.com>
2024-04-19 17:19:35 +0900

    update

--- src/contexts/QuizContext.js
+++ src/contexts/QuizContext.js
@@ -1,26 +1,25 @@
 import React, { createContext, useState } from "react";
-import quizes from "../quizes.json"
-import { useNavigate } from "react-router-dom";
+import quizes from "../quizes.json";
 
-export const QuizContext = createContext()
+export const QuizContext = createContext();
 
 export const QuizProvider = ({ children }) => {
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const [searchQuery, setSearchQuery] = useState('');
+  const clearSearchQuery = () => {
+    setSearchQuery("");
+  };
 
-    const navigate = useNavigate()
-
-    const clearSearchQuery = () => {
-        setSearchQuery('');
-    };
-
-
-    return (
-        <QuizContext.Provider value={{
-            quizes, searchQuery, clearSearchQuery, setSearchQuery
-        }}>
-            {children}
-        </QuizContext.Provider>
-    )
-
-}
+  return (
+    <QuizContext.Provider
+      value={{
+        quizes,
+        searchQuery,
+        clearSearchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </QuizContext.Provider>
+  );
+};
